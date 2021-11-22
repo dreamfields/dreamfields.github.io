95d21e60b2f13b1e8f2caaeacf91a3c48d7e4392
DreamFields <1339611623@qq.com>
2021-11-22 16:47:06 +0800

    切换主题-基础功能finish

--- finish.bash
+++ finish.bash
@@ -1,5 +1,10 @@
+###
+ # @Author: Meng Tian
+ # @Date: 2021-11-21 14:52:26
+ # @Description: Do not edit
+### 
 hexo g
 hexo d
 git add .
-git commit -m 'update'
+git commit -m '切换主题-基础功能finish'
 git push
